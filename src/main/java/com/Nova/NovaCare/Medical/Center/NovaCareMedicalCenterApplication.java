fd70931c93c74e79c018e34d8504b88aacc78b5d
BalaganapathiTech <balaganapathi33@gmail.com>
2024-12-04 13:15:32 +0530

    change some configuration

--- src/main/java/com/Nova/NovaCare/Medical/Center/NovaCareMedicalCenterApplication.java
+++ src/main/java/com/Nova/NovaCare/Medical/Center/NovaCareMedicalCenterApplication.java
@@ -1,9 +1,13 @@
 package com.Nova.NovaCare.Medical.Center;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+
+import java.util.Collections;
 @SpringBootApplication
 public class NovaCareMedicalCenterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(NovaCareMedicalCenterApplication.class, args);
+		SpringApplication app = new SpringApplication(NovaCareMedicalCenterApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+		app.run(args);
 	}
 }
